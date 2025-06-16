8253290cb2d265640835d1c1fe11eae7a5508eda
Mahabubul Alam Arif <mahabubulalamarif@gmail.com>
2025-06-17 01:21:18 +0600

    env added and updated jwt

--- src/Firebase/Firebase.config.js
+++ src/Firebase/Firebase.config.js
@@ -5,13 +5,20 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyCH-YyX2MekCHvaQLL6e6EqJlBYmDatEjc",
-    authDomain: "foods-app-704bd.firebaseapp.com",
-    projectId: "foods-app-704bd",
-    storageBucket: "foods-app-704bd.firebasestorage.app",
-    messagingSenderId: "667644319055",
-    appId: "1:667644319055:web:18028aea10a32765a5613a"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
+
+// apiKey: "AIzaSyCH-YyX2MekCHvaQLL6e6EqJlBYmDatEjc",
+    // authDomain: "foods-app-704bd.firebaseapp.com",
+    // projectId: "foods-app-704bd",
+    // storageBucket: "foods-app-704bd.firebasestorage.app",
+    // messagingSenderId: "667644319055",
+    // appId: "1:667644319055:web:18028aea10a32765a5613a"
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
